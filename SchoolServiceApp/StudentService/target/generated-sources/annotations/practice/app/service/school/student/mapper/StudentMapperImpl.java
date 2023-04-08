a119d931f2d2bba0f628db5ebde5dae5ecd5f3b0
EmreOkyay <okyayatilla@hotmail.com>
2023-04-08 18:04:55 +0300

    created parent pom and distributed dependencies

---
+++ SchoolServiceApp/StudentService/target/generated-sources/annotations/practice/app/service/school/student/mapper/StudentMapperImpl.java
@@ -0,0 +1,31 @@
+package practice.app.service.school.student.mapper;
+
+import com.okyay.schoolapp.data.entity.Student;
+import javax.annotation.processing.Generated;
+import practice.app.service.school.student.dto.StudentDTO;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2023-04-08T17:51:59+0300",
+    comments = "version: 1.5.3.Final, compiler: javac, environment: Java 17.0.2 (Oracle Corporation)"
+)
+public class StudentMapperImpl implements IStudentMapper {
+
+    @Override
+    public StudentDTO toStudentDTO(Student student) {
+        if ( student == null ) {
+            return null;
+        }
+
+        StudentDTO studentDTO = new StudentDTO();
+
+        studentDTO.studentNo = student.getStudentNo();
+        studentDTO.firstName = student.getFirstName();
+        studentDTO.middleName = student.getMiddleName();
+        studentDTO.lastName = student.getLastName();
+        studentDTO.className = student.getClassName();
+        studentDTO.gpa = student.getGpa();
+
+        return studentDTO;
+    }
+}
---
+++ SchoolServiceApp/TeacherService/target/generated-sources/annotations/practice/app/service/school/teacher/mapper/TeacherMapperImpl.java
@@ -0,0 +1,30 @@
+package practice.app.service.school.teacher.mapper;
+
+import com.okyay.schoolapp.data.entity.Teacher;
+import javax.annotation.processing.Generated;
+import practice.app.service.school.teacher.dto.TeacherDTO;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2023-04-08T18:00:35+0300",
+    comments = "version: 1.5.3.Final, compiler: javac, environment: Java 17.0.2 (Oracle Corporation)"
+)
+public class TeacherMapperImpl implements ITeacherMapper {
+
+    @Override
+    public TeacherDTO toTeacherDTO(Teacher teacher) {
+        if ( teacher == null ) {
+            return null;
+        }
+
+        TeacherDTO teacherDTO = new TeacherDTO();
+
+        teacherDTO.teacher_no = teacher.getTeacher_no();
+        teacherDTO.firstName = teacher.getFirstName();
+        teacherDTO.middleName = teacher.getMiddleName();
+        teacherDTO.lastName = teacher.getLastName();
+        teacherDTO.registerDate = teacher.getRegisterDate();
+
+        return teacherDTO;
+    }
+}
